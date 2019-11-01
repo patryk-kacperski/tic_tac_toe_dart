02a4ec5b1025e14e1e78a2d01799201ef4313955
Patryk Kacperski <patryk.kacperski0@gmail.com>
2019-11-01 11:14:46 +0100

    Implemented ItemPlacer class

---
+++ lib/components/item_placer.dart
@@ -0,0 +1,31 @@
+import 'dart:math';
+
+import 'package:tic_tac_toe/components/board_inputs.dart';
+import 'package:tic_tac_toe/components/item_placer_inputs.dart';
+import 'package:tic_tac_toe/enums/board_item_type.dart';
+import 'package:tic_tac_toe/enums/placement_result.dart';
+
+class ItemPlacer implements ItemPlacerInputs {
+  @override
+  PlacementResult canPlace(
+    BoardItemType itemType,
+    Point<int> point,
+    BoardInputs board,
+  ) {
+    if (!board.containts(point)) {
+      return PlacementResult.outOfBoard;
+    }
+    if (board.getItemAt(point) != BoardItemType.none) {
+      return PlacementResult.occupied;
+    }
+    if (itemType == BoardItemType.none) {
+      return PlacementResult.nonePassed;
+    }
+    return PlacementResult.valid;
+  }
+
+  @override
+  void place(BoardItemType itemType, Point<int> point, BoardInputs board) {
+    board.setItemAt(itemType, point);
+  }
+}
